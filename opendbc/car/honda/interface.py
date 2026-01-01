b090611e029904a564f049bf7a677b65ed96345e
nrdr <81265253+nrdr@users.noreply.github.com>
2026-01-01 01:22:44 -0500

    Long Tuning

--- opendbc/car/honda/interface.py
+++ opendbc/car/honda/interface.py
@@ -96,9 +96,9 @@ class CarInterface(CarInterfaceBase):
       if candidate in HONDA_BOSCH_RADARLESS:
         ret.stopAccel = CarControllerParams.BOSCH_ACCEL_MIN  # stock uses -4.0 m/s^2 once stopped but limited by safety model
     else:
-      # Gentle Following / Honda Alt Tune
-      ret.longitudinalTuning.kiBP = [0., 5., 35.]
-      ret.longitudinalTuning.kiV = [1.2, 0.8, 0.5]
+      # Tuned 01.01.2026 by Aragon7777 for stopping smoothness
+      ret.longitudinalTuning.kiBP = [0., 1.5, 5., 35.]
+      ret.longitudinalTuning.kiV  = [0.2, 0.6, 0.8, 0.5]
       # # default longitudinal tuning for all hondas
       # ret.longitudinalTuning.kiBP = [0., 5., 35.]
       # ret.longitudinalTuning.kiV = [1.2, 0.8, 0.5]
