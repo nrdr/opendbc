8bd237b63da9179e514c2a503c1eaea970505467
nrdr <81265253+nrdr@users.noreply.github.com>
2026-01-11 00:29:45 -0500

    Bump

--- opendbc/car/honda/values.py
+++ opendbc/car/honda/values.py
@@ -35,7 +35,7 @@ class CarControllerParams:
   BOSCH_ACCEL_MAX = 2.0  # m/s^2
 
   BOSCH_GAS_LOOKUP_BP = [0.0, 2.0]  # 2m/s^2
-  BOSCH_GAS_LOOKUP_V = [0, 1600]
+  BOSCH_GAS_LOOKUP_V = [0, 750]
 
   STEER_STEP = 1  # 100 Hz
   STEER_DELTA_UP = 3  # min/max in 0.33s for all Honda
