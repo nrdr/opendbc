fdb86be570a8e96da3ea69a0473107eea68eb08e
nrdr <81265253+nrdr@users.noreply.github.com>
2026-01-13 09:45:04 -0500

    Revert "Update carcontroller.py"
    This reverts commit ddfe2843d1865005c61783621d92c39d23e9268e.

--- opendbc/car/honda/carcontroller.py
+++ opendbc/car/honda/carcontroller.py
@@ -136,13 +136,13 @@ class CarController(CarControllerBase, MadsCarController, GasInterceptorCarContr
     # - while steeringPressed (and a short time after), force OP steer torque to 0
     #   and reset filter state so it doesn't "push back"
     self.driver_override_until_nanos = 0
-    self.override_hold_s = 0.2  # try 0.20–0.50
+    self.override_hold_s = 0.35  # try 0.20–0.50
 
   def update(self, CC, CC_SP, CS, now_nanos):
     MadsCarController.update(self, self.CP, CC, CC_SP)
     actuators = CC.actuators
     hud_control = CC.hudControl
-    hud_v_cruise = 255
+    hud_v_cruise = hud_control.setSpeed / CS.v_cruise_factor if hud_control.speedVisible else 255
     pcm_cancel_cmd = CC.cruiseControl.cancel
 
     if len(CC.orientationNED) == 3:
